bd75a93c32f35a8e474d66918e937674d8dbbe81
zewen315 <zewen315@gmail.com>
2026-07-27 15:16:46 -0400

    permute improved

--- permutations.py
+++ permutations.py
@@ -17,7 +17,9 @@ def permute(nums: List[int]) -> List[List[int]]:
 
             used[i] = True
             path.append(nums[i])
+
             helper(path)
+            
             path.pop()
             used[i] = False
 
@@ -25,7 +27,42 @@ def permute(nums: List[int]) -> List[List[int]]:
     return res
 
 
+def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+    nums.sort()
+
+    res = []
+    path = []
+    used = [False] * len(nums)
+
+    def dfs():
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+
+        for i in range(len(nums)):
+            if used[i]:
+                continue
+
+            # Skip duplicates on the same level
+            if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                continue
+
+            used[i] = True
+            path.append(nums[i])
+
+            dfs()
+
+            path.pop()
+            used[i] = False
+
+        dfs()
+        return res
+
+
 if __name__ == "__main__":
     nums = [1, 2, 3]
-
     print(permute(nums))
+
+    nums = [1, 1, 2]
+    print(permute(nums))
+    print(permuteUnique(nums))
